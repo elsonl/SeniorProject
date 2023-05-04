24270503d9a13952e92a49433a764909f1854614
elson <dragon18rules@gmail.com>
2023-05-04 15:39:49 -0400

    Initial Commit

---
+++ SeniorProject/SeniorProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorProjectApp.swift
+//  SeniorProject
+//
+//  Created by Elson Lin on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SeniorProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
